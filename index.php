7f5142b7644d22ab1d2d542c34ec570e9be079e2
Manfred Kutas <manfred@z-push.org>
2016-12-13 17:21:26 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/***********************************************
+* File      :   index.php
+* Project   :   KopanoDAV
+* Descr     :   This is the entry point
+*               through which all requests
+*               are processed.
+*
+* Created   :   13.12.2016
+*
+* Copyright 2016 Kopano b.v.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License, version 3,
+* as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Consult LICENSE file for details
+************************************************/
+
+namespace Kopano\DAV;
+
